16df5f9450935c1ee8b5f9ba4788e7f3e553d393
Austin Kerbow <amkerbow@gmail.com>
2018-06-20 04:18:38 -0700

    Add more options to compile script.
    Allows specifying a target architecture when building LLVM. Adds more
    descriptive messages describing script prompts. Lets users decide
    whether or not to install LLVM and clang after building.

--- CompileLLVMandDRAGONEGG.sh
+++ CompileLLVMandDRAGONEGG.sh
@@ -7,54 +7,76 @@ DRAGONEGGDIR="$BASEDIR/dragonegg"
 CPU2006DIR="$BASEDIR/CPU2006"
 GCCPATH="/usr/bin/gcc-4.8"
 
-echo -n 'Install Prerequisites? [y/n]: '
-read needPrereq
+read -p 'Install Prerequisites on Ubuntu? [y/n] (default: n): ' -r
 
-if [ $needPrereq == 'y' ]; then
-echo 'Installing Prerequisites'
-sudo apt-get install cmake
-sudo apt-get install gcc-4.8
-sudo apt-get install gcc-4.8-plugin-dev
-sudo apt-get install gfortran-4.8
-sudo apt-get install g++-4.8
+if [[ $REPLY =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+  echo 'Installing Prerequisites'
+  sudo apt-get install cmake
+  sudo apt-get install gcc-4.8
+  sudo apt-get install gcc-4.8-plugin-dev
+  sudo apt-get install gfortran-4.8
+  sudo apt-get install g++-4.8
 fi
 
-echo -n 'Build release version? [y/n]: '
-read isRelease
+read -p 'Build release version? [y/n] (default: y): ' -r
 
-echo 'building LLVM'
+if [[ $REPLY =~ ^([yY][eE][sS]|[yY])+$ || -z "$REPLY" ]]
+then
+  isRelease='True'
+else
+  isRelease='False'
+fi
 
-if [ $isRelease == 'y' ]; then
-  if [ ! -d "$LLVMDIR/release_build" ]; then
+read -p 'Choose target architecture (default: X86): ' -r targetArch
+if [ -z "$targetArch" ]
+then
+  targetArch='X86'
+fi
+echo "Targeting $targetArch"
+
+# Create build directory and call cmake
+if [ $isRelease == 'True' ]
+then
+  if [ ! -d "$LLVMDIR/release_build" ]
+  then
     mkdir $LLVMDIR/release_build
   fi
   cd $LLVMDIR/release_build
-  cmake -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE="Release" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON $LLVMDIR/llvm-master
+  cmake -DLLVM_TARGETS_TO_BUILD=$targetArch -DCMAKE_BUILD_TYPE="Release" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON $LLVMDIR/llvm-master
   cp compile_commands.json $BASEDIR
 
 else
-  if [ ! -d "$LLVMDIR/build" ]; then
+  if [ ! -d "$LLVMDIR/build" ]
+  then
     mkdir $LLVMDIR/build
   fi
   # The default for cmake is the debug build
   cd $LLVMDIR/build
-  cmake -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON $LLVMDIR/llvm-master
+  cmake -DLLVM_TARGETS_TO_BUILD=$targetArch -DCMAKE_EXPORT_COMPILE_COMMANDS=ON $LLVMDIR/llvm-master
   cp compile_commands.json $BASEDIR
 fi
 
+# Build LLVM/clang
+echo 'Building LLVM'
 make
-sudo cmake --build . --target install
+
+read -p 'Install LLVM/clang? [y/n] (default: n): ' -r
+if [[ $REPLY =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+  sudo cmake --build . --target install
+fi
 
 cd $BASEDIR
 
-echo -n 'Build dragonegg? [y/n]: '
-read buildDragonegg
-
-if [ $buildDragonegg == 'y' ]; then
+echo 'Note: This assumes you are using Ubuntu and you installed the prerequisites.'
+read -p 'Build dragonegg? [y/n] (default: n): ' -r
+if [[ $REPLY =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
   echo 'building dragonegg'
   cd $DRAGONEGGDIR
   make clean
-  if [ $isRelease == 'y' ]; then
+  if [ $isRelease == 'True' ]; then
     GCC=$GCCPATH LLVM_CONFIG=$LLVMDIR/release_build/bin/llvm-config make -B
   else
     GCC=$GCCPATH LLVM_CONFIG=$LLVMDIR/build/bin/llvm-config make -B
