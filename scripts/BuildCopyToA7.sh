b4b99ab421997c761a40f686adeeec36b97e2b9a
Austin Kerbow <amkerbow@gmail.com>
2018-08-24 05:06:57 -0700

    ARM: Update CPU2006 tools to automate running on A7.

---
+++ scripts/BuildCopyToA7.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#Build benchmarks and copy them to the A7 machine.
+
+BENCH="401.bzip2 429.mcf 433.milc 445.gobmk 456.hmmer 458.sjeng 462.libquantum 464.h264ref 470.lbm 482.sphinx3  444.namd 447.dealII 453.povray 471.omnetpp 473.astar"
+
+# source the shrc
+. ./shrc
+
+echo 'Cleaning benchmarks'
+runspec --loose -size=test -iterations=1 -config=Intel_llvm_3.9.cfg --tune=base -r 1 -I -a scrub $BENCH 2>&1 > /dev/null
+
+echo 'Building benchmarks'
+runspec --loose -size=test -iterations=1 -config=Intel_llvm_3.9.cfg --tune=base -r 1 -I -a build $BENCH 2>&1 > /dev/null
+
+echo 'Creating fake run directories'
+#runspec --fake --loose --size test --tune base --config Intel_llvm_3.9.cfg $BENCH
+cd ./benchspec/CPU2006/
+
+echo 'Creating archive'
+tar cJf ziped_benches.tar.xz */exe
+
+echo 'Copying to A7 machine'
+scp -q ziped_benches.tar.xz ghassan@99.113.71.118:~
+
+echo 'Done!'
